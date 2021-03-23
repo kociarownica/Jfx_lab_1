4d85ea125bb2e397bb05268219c69bf72bdfbd1b
kociarownica <anastasiyaholupova@gmail.com>
2021-03-23 10:50:04 +0300

    message

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -31,49 +31,75 @@ public class App extends Application {
         primaryStage.setTitle("Time");
         primaryStage.setScene(new Scene(root, 780, 275));
 
-        //first TextField "Distance"
         root.setPadding(new Insets(10,10,10,10));
         root.setHgap(5);
         root.setVgap(5);
+
+        TextField distance = createTextDistance(root);          //first TextField "Distance"
+        TextField speed = createTextSpeed(root);                //second TextField "Speed"
+
+        Label labelDistance = createLabelDistance(root);        // label distance
+        Label labelSpeed = createLabelSpeed(root);              // label speed
+        Label labelPTime = createLabelPTime(root);               // label passing time
+        Label timeLabel = createTimeLabel(root);                 // label time
+
+        addButton(root, distance, speed, timeLabel);
+
+        primaryStage.show();
+    }
+
+    TextField createTextDistance(GridPane root){
         TextField distance = new TextField();
         distance.setPromptText("distance");
         GridPane.setConstraints(distance,11,10);
         root.getChildren().add(distance);
+        return distance;
+    }
 
-        //second TextField "Speed"
+    TextField createTextSpeed(GridPane root){
         TextField speed = new TextField();
         speed.setPromptText("speed");
         GridPane.setConstraints(speed,50,10);
         root.getChildren().add(speed);
+        return speed;
+    }
 
-        // label distance
+    Label createLabelDistance(GridPane root){
         Label labelDistance = new Label();
         GridPane.setConstraints(labelDistance, 10, 10);
         labelDistance.setText("Расстояние");
         labelDistance.setPrefSize(80,20);
         root.getChildren().add(labelDistance);
+        return labelDistance;
+    }
 
-        // label speed
+    Label createLabelSpeed(GridPane root){
         Label labelSpeed = new Label();
         GridPane.setConstraints(labelSpeed, 35, 10);
         labelSpeed.setText("Скорость");
         labelSpeed.setPrefSize(130,20);
         root.getChildren().add(labelSpeed);
+        return labelSpeed;
+    }
 
-        // label passing time
+    Label createLabelPTime(GridPane root){
         Label labelPTime = new Label();
         GridPane.setConstraints(labelPTime, 10, 20);
         labelPTime.setText("Время прохождения");
         labelPTime.setPrefSize(210,20);
         root.getChildren().add(labelPTime);
+        return labelPTime;
+    }
 
-        // label time
+    Label createTimeLabel(GridPane root){
         Label timeLabel = new Label();
         GridPane.setConstraints(timeLabel, 20, 20);
         //timeLabel.setText("Время прохождения");
         timeLabel.setPrefSize(120,20);
         root.getChildren().add(timeLabel);
-
+        return timeLabel;
+    }
+    void addButton(GridPane root, TextField distance, TextField speed, Label timeLabel){
         // button
         Button button = new Button();
         GridPane.setConstraints(button, 50, 20);
@@ -87,10 +113,7 @@ public class App extends Application {
             timeLabel.setText(str);
         });
         root.getChildren().add(button);
-
-        primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
